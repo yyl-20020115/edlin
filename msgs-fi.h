a66d982a69b4b130e07338bd10faeca9ce5dfa65
Yilin Yang <yyl_20050115@hotmail.com>
2024-11-04 15:07:15 +0800

    添加项目文件。

---
+++ msgs-fi.h
@@ -0,0 +1,60 @@
+/* edlin.fi - messages file (Finnish version)                                  */
+/*                                                                             */
+/* Author: Gregory Pietsch                                                     */
+/*                                                                             */
+/* DESCRIPTION:                                                                */
+/*                                                                             */
+/* This file contains #defines for all the message strings in edlin.           */
+/* For internationalization fun, just translate the messages in this           */
+/* file.                                                                       */
+/*                                                                             */
+/*                                                                             */
+
+#ifndef MSGS_H
+#define MSGS_H
+
+#define YES	"Kk"
+
+#define G00001  " : "
+#define G00002  "O.K.? "
+#define G00003  "Sy\224tevirhe"
+#define G00004  "%s: %lu rivi luettu\n"
+#define G00005  "%s: %lu rivi\204 luettu\n"
+#define G00006  "%s: %lu rivi kirjoitettu\n"
+#define G00007  "%s: %lu rivi\204 kirjoitettu\n"
+#define G00008  "%lu:%c%s\n"
+#define G00009  "Paina <Enter> jatkaaksesi"
+#define G00010  "%lu: "
+#define G00011  "Ei l\224ydy"
+#define G00012  "%lu: %s\n"
+#define G00013  "\nedlin sis\204lt\204\204 seuraavat komennot:\n"
+#define G00014  "#                 muokkaa rivi\204         [#],[#],#m        siirr\204"
+#define G00015  "a                 lis\204\204 loppuun         [#][,#]p          sivu"
+#define G00016  "[#],[#],#,[#]c    kopioi                q                 lopeta (hylk\204\204)"
+#define G00017  "[#][,#]d          poista                [#][,#][?]r$,$    korvaa"
+#define G00018  "e<>               tallenna ja lopeta    [#][,#][?]s$      hae"
+#define G00019  "[#]i              sy\224t\204                 [#]t<>            yhdist\204"
+#define G00020  "[#][,#]l          luettelo              [#]w<>            kirjoita\n"
+#define G00021  "miss\204 $ on merkkijono, <> tiedostonimi,"
+#define G00022  "# on luku (tai .=nykyinen rivi, $=viimeinen rivi,"
+#define G00023  "tai jompikumpi + tai - toinen luku).\n"
+#define G00024  ", (c) 2003 Gregory Pietsch"
+#define G00025  "This program comes with ABSOLUTELY NO WARRANTY."
+#define G00026  "It is free software, and you are welcome to redistribute it"
+#define G00027  "under the terms of the GNU General Public License -- either"
+#define G00028  "version 2 of the license, or, at your option, any later"
+#define G00029  "version.\n"
+#define G00030  "Muisti loppu"
+#define G00031  "Merkkijonopituus virheellinen"
+#define G00032  "Merkkijonopaikka virheellinen"
+#define G00033  "Sy\224te virheellinen, ? avaa ohjeen."
+#define G00034  "Ei tiedostonime\204"
+#define G00035  "Puskuri liian iso"
+#define G00036  "Puskuripaikka virheellinen"
+#define G00037  "VIRHE: %s\n"
+
+/* END OF FILE                                                                 */
+
+#endif
+
+/* END OF FILE */
